ded1d336637468b617069dfb4f07d683eeb24de5
RoseAnnGregorio <148958372+RoseAnnGregorio@users.noreply.github.com>
2023-10-25 17:10:53 +0800

    Animal.java

---
+++ Animal.java
@@ -0,0 +1,11 @@
+class Animals{
+    String name;
+
+    public Animals(String name) {
+        this.name = name;
+    }
+
+    public void sound() {
+        System.out.println(name + " makes a sound.");
+    }
+}
