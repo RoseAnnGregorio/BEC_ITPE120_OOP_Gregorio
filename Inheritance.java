b81b7ca56b725c5165cf94b1f52879b0b5353771
RoseAnnGregorio <148958372+RoseAnnGregorio@users.noreply.github.com>
2023-10-25 17:12:48 +0800

    Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+public class Inheritance{
+    public static void main(String[] args) {
+       
+        Cat myCat = new Cat("Powie", "Persian");
+
+        
+        myCat.sound();
+
+        myCat.eat();
+    }
+}
